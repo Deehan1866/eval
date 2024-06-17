fdd7e82a292f53b699305a1fc76a58cfadf43c0d
Deehan1866 <105232241+Deehan1866@users.noreply.github.com>
2024-06-17 18:33:42 +0600

    Update run_qa.py

--- retrieval_qa/run_qa.py
+++ retrieval_qa/run_qa.py
@@ -509,7 +509,7 @@ def main():
         references = [{"id": ex["id"], "answers": ex[answer_column_name]} for ex in examples]
         return EvalPrediction(predictions=formatted_predictions, label_ids=references)
 
-    metric = load_metric("squad_v2" if data_args.version_2_with_negative else "squad")
+    metric = load_metric("squad_v2" if data_args.version_2_with_negative else "squad", trust_remote_code=True)
 
     def compute_metrics(p: EvalPrediction):
         if "phrase_sense_disambiguation" not in data_args.dataset_name.lower():
