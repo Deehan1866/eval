ce7a6b56b7aaae89a44bfd297306976c86ac0b5a
Deehan1866 <105232241+Deehan1866@users.noreply.github.com>
2024-06-18 16:11:56 +0600

    Update model_path.py

--- similarity/model_path.py
+++ similarity/model_path.py
@@ -8,5 +8,9 @@ MODEL_PATH = {
     #"SimCSE": "princeton-nlp/sup-simcse-bert-base-uncased",
     #"E5": "intfloat/e5-base-v2"
     #"EMistral": "intfloat/e5-mistral-7b-instruct"  #not working
+    #"Mistral": "mistralai/Mistral-7B-v0.1" #same issue as the previous one
+    #"Xlnet": "xlnet/xlnet-base-cased" #OverflowError: can't convert negative int to unsigned
+    #"Albert": "albert/albert-base-v2" #many many phrases are not found! accuracy is really low
+    "T511B": "google-t5/t5-11b"
 }
 
