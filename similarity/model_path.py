4f5c9e4a0547b9574472d0ae2b03cd7076ce6c08
Deehan1866 <105232241+Deehan1866@users.noreply.github.com>
2024-06-18 21:36:23 +0600

    Update model_path.py

--- similarity/model_path.py
+++ similarity/model_path.py
@@ -11,6 +11,15 @@ MODEL_PATH = {
     #"Mistral": "mistralai/Mistral-7B-v0.1" #same issue as the previous one
     #"Xlnet": "xlnet/xlnet-base-cased" #OverflowError: can't convert negative int to unsigned
     #"Albert": "albert/albert-base-v2" #many many phrases are not found! accuracy is really low
-    "T511B": "google-t5/t5-11b"
+    #"T511B": "google-t5/t5-11b" #too large
+    #"Distilbert": "distilbert/distilbert-base-uncased"  #GOOD
+    #"DistilbertFinetuned": "distilbert/distilbert-base-uncased-finetuned-sst-2-english" #62 accuracy
+    #"DistilRoberta": "distilbert/distilroberta-base"
+    #"DistilbertQA": "distilbert/distilbert-base-cased-distilled-squad"
+    #"LLM": "meta-llama/Meta-Llama-3-8B" #too big
+    #"Roberta": "FacebookAI/roberta-base" #too many phrase not found
+    #"XLMRoberta": "FacebookAI/xlm-roberta-base" #too many phrase not found bt still gives 58
+    #"RobertaEnglish": "FacebookAI/xlm-roberta-large-finetuned-conll03-english" #too many
 }
+
 
